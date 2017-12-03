7c287f9827bf3cae60ab15b8aef9517cf4944018
Myy Miouyouyou <myy@miouyouyou.fr>
2017-12-03 22:55:57 +0100

    The libArcane and its secrets, now available on ARM64 !
    Signed-off-by: Myy Miouyouyou <myy@miouyouyou.fr>

--- decypherArcane.s
+++ decypherArcane.s
@@ -10,39 +10,48 @@ secret:
 	.hword 55357, 56377, 12495, 12512, 12473, 12479, 12540, 10
 	.hword 55357, 56360, 27193, 29066, 10
 	.hword 55357, 56445, 65, 110, 32, 97, 108, 105, 101, 110, 10
-secret_len = . - secret
-secret_len = secret_len / 2  /* 2 bytes per character */
+secret_len = (. - secret) / 2
 
 .text
 .align 2
 .globl Java_adventurers_decyphering_secrets_decyphapp_DecypherActivity_decypherArcaneSecrets
 .type Java_adventurers_decyphering_secrets_decyphapp_DecypherActivity_decypherArcaneSecrets, %function
 Java_adventurers_decyphering_secrets_decyphapp_DecypherActivity_decypherArcaneSecrets:
-	push {r4-r7, lr} // Prologue. We will use r4, r5, r6, r7
+	sub sp, sp, 48 // Prepare to push x19, x20, x21, x22 and lr (x30)
+	               // 5 registers of 8 bytes each -> 40 bytes
+	               // Unless you like to deal with corner cases, you'll
+	               // have to keep the stack aligned on 16 bytes.
+	               // 40 % 16 != 0 but 48 % 16 == 0, so we use 48 bytes.
+	stp x19, x20, [sp]
+	stp x21, x22, [sp, 16]
+	stp x23, x30, [sp, 32]
 
-	// Passed parameters - r0 : *_JNIEnv, r1 : thisObject
+	// Passed parameters - x0 : *_JNIEnv, x1 : thisObject
 
-	mov r4, r0   // r4 : Backup of *JNIEnv as we'll use it very often
-	mov r5, r1   // r5 : Backup of thisObject as we'll invoke methods on it
-	ldr r6, [r0] // r6 : Backup of *_JNINativeInterface, located at *_JNIEnv,
-	             //      since we'll also use it a lot
+	mov x19, x0   // x19 <- Backup of *JNIEnv as we'll use it very often
+	mov x20, x1   // x20 <- Backup of thisObject as we'll invoke methods on it
+	ldr x21, [x0] // x21 <- Backup of *_JNINativeInterface, located at *_JNIEnv,
+	              //      since we'll also use it a lot
 
-	/* Preparing to call NewString(*_JNIEnv : r0, 
-	                     *string_characters : r1, 
-	                          string_length : r2).
-	   *_JNIEnv is still in r0.
+	/* Preparing to call NewString(*_JNIEnv : x0, 
+	                     *string_characters : x1, 
+	                          string_length : x2).
+	   *_JNIEnv is still in x0.
 	*/
 
-	ldr r1, =secret     // r1 : The UTF16-LE characters composing 
-	                    //      the java.lang.String
-	mov r2, #secret_len // r2 : The length of the String
-	ldr r3, [r6, #652]  // Get *JNINativeInterface->NewString. 
-	                    // +652 is NewString's offset
-	blx r3              // secret_string : r0 <- NewString(*_JNIEnv : r0, 
-	                    //                       *string_characters : r1,
-	                    //                            string_length : r2)
-	mov r7, r0          // r7 : secret_string
-	                    // Keep the returned string for later use
+	adr x1, secret       // x1 <- *secret : The UTF16-LE characters composing 
+	                     //                 the java.lang.String we'll pass to
+	                     //                 the Java method called afterwards.
+	mov x2, #secret_len  // x2 <- secret_len : The length of that java.lang.String
+	ldr x3, [x21, #1304] // x3 <- *JNINativeInterface->NewString function. 
+	                     // +1304 is NewString's offset in the JNINativeInterface
+	                     // structure.
+	blr x3               // secret_java_string : x0 <- NewString(*_JNIEnv : x0, 
+	                     //                                       *secret : x1,
+	                     //                                    secret_len : x2)
+
+	mov x22, x0          // x22 <- secret_java_string
+	                     // Keep the returned string for later use
 
 	/* Calling showText(java.lang.String) through the JNI
 	
@@ -51,48 +60,50 @@ Java_adventurers_decyphering_secrets_decyphapp_DecypherActivity_decypherArcaneSe
 	   to get it.
 	*/
 
-	// Preparing to call GetObjectClass(*_JNIEnv : r0, thisObject : r1)
-	mov r0, r4         // r0 : *_JNIEnv
-	mov r1, r5         // r1 : thisObject
-	ldr r2, [r6, #124] // Get *JNINativeInterface->GetObjectClass (+124)
-	blx r2             // jclass : r0 <- GetObjectClass(*JNIEnv : r0, 
-	                   //                            thisObject : r1)
+	// Preparing to call GetObjectClass(*_JNIEnv : x0, thisObject : x1)
+	mov x0, x19         // x0 <- *_JNIEnv (previously saved in x19)
+	mov x1, x20         // x1 <- thisObject (previously saved in x20)
+	ldr x2, [x21, #248] // x2 <- Get *JNINativeInterface->GetObjectClass (*JNINativeInterface+248)
+	blr x2              // jclass : x0 <- GetObjectClass(*JNIEnv : x0, 
+	                    //                            thisObject : x1)
 	/* Second : We need the JNI ID of the method we want to call
-	   Preparing for GetMethodId(*JNIEnv : r0, 
-	                              jclass : r1, 
-	                         method_name : r2, 
-	                    method_signature : r3)
+	   Preparing for GetMethodId(*JNIEnv : x0, 
+	                              jclass : x1, 
+	                         method_name : x2, 
+	                    method_signature : x3)
 	*/
 
-	mov r1, r0 // r1 : jclass returned by GetObjectClass
-	mov r0, r4 // r0 : *JNIEnv, previously backed up in r4
-	ldr r2, =java_method_name      // r2 : The method name
-	ldr r3, =java_method_signature // r3 : The method signature
-	ldr r8, [r6, #132]             // Get *JNINativeInterface->GetMethodId
-	                               // (+132)
-	blx r8     // revealTheSecretID : r0 <- GetMethodId(*_JNIEnv : r0, 
-	           //                                         jclass : r1, 
-	           //                                    method_name : r2, 
-	           //                               method_signature : r3)
+	mov x1, x0  // x1 <- jclass returned by GetObjectClass
+	mov x0, x19 // x0 <- *JNIEnv, previously backed up in x19
+	adr x2, java_method_name      // x2 <- &java_method_name : The method name
+	adr x3, java_method_signature // x3 <- &java_method_signature : The method signature
+	ldr x4, [x21, #264]           // Get *JNINativeInterface->GetMethodId
+
+	blr x4     // revealTheSecretID : x0 <- GetMethodId(*_JNIEnv : x0, 
+	           //                                         jclass : x1, 
+	           //                                   &method_name : x2, 
+	           //                              &method_signature : x3)
 
 	// Finally : Call the method. Since it's a method returning void, 
 	// we'll use CallVoidMethod.
-	// Preparing to call CallVoidMethod(*_JNIEnv : r0, 
-	//                                thisObject : r1,
-	//                         revealTheSecretID : r2,
-	//                             secret_string : r3)
+	// Preparing to call CallVoidMethod(*_JNIEnv : x0, 
+	//                                thisObject : x1,
+	//                         revealTheSecretID : x2,
+	//                             secret_string : x3)
 
-	mov r2, r0         // r2 : revealTheSecretID
-	mov r1, r5         // r1 : thisObject
-	mov r0, r4         // r0 : *_JNIEnv
-	mov r3, r7         // r3 : secret_string
-	ldr r8, [r6, #244] // Get *_JNINativeInterface->CallVoidMethod (+244).
-	blx r8 // CallVoidMethod(*_JNIEnv : r0, 
-	       //              thisObject : r1,
-	       //       revealTheSecretID : r2,
-	       //              the_string : r3)
+	mov x2, x0          // x2 <- revealTheSecretID
+	mov x1, x20         // x1 <- thisObject (previously saved in x20)
+	mov x0, x19         // x0 <- *_JNIEnv (previously saved in x19)
+	mov x3, x22         // x3 <- secret_java_string (previously saved in x22)
+	ldr x4, [x21, #488] // x4 <- *_JNINativeInterface->CallVoidMethod (+488).
+	blr x4 // CallVoidMethod(*_JNIEnv : x0, 
+	       //              thisObject : x1,
+	       //       revealTheSecretID : x2,
+	       //              the_string : x3)
 	       // => Java : revealTheSecret(the_string)
 
-	pop {r4-r7, pc} // Restoring the scratch-registers and 
-	                // returning by loading the link-register 
-	                // into the program-counter
+	ldp x19, x20, [sp]
+	ldp x21, x22, [sp, 16]
+	ldp x23, x30, [sp, 32]
+	add sp, sp, 48
+	ret
